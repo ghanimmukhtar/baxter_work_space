f244b90a03da82a33d7f136425dbb58b964868c8
ghanim <mukhtar@isir.upmc.fr>
2016-07-13 17:22:20 +0200

    tested node

--- src/baxter_work_space.cpp
+++ src/baxter_work_space.cpp
@@ -9,36 +9,11 @@
 #include <boost/timer.hpp>
 #include <moveit/kinematic_constraints/utils.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
+#include <baxter_core_msgs/JointCommand.h>
 
 std::vector<double> q(7);
-
-//This is a function that takes as inputs: a character that name an axis (x, y or z), and a float that represents an angle of rotation about that axis. It returns the corresponding rotation matrix (3x3)
-Eigen::Matrix3d Rot(char axis, float angle) {
-    Eigen::Matrix3d RotX,RotY,RotZ,empty_Rot;
-    if (axis == 'x'){
-        RotX << 1,          0,           0,
-                0, cos(angle), -sin(angle),
-                0, sin(angle),  cos(angle);
-        return RotX;
-    }
-    else if (axis == 'y'){
-        RotY << cos(angle),  0, sin(angle),
-                         0,  1,          0,
-               -sin(angle),  0, cos(angle);
-        return RotY;
-    }
-    else if (axis == 'z'){
-        RotZ << cos(angle), -sin(angle), 0,
-                sin(angle),  cos(angle), 0,
-                         0,           0, 1;
-        return RotZ;
-    }
-    else{
-        std::cout << "enter valid coordinate: X, Y or Z -------------------------------------------------" << std::endl;
-        return empty_Rot;
-    }
-}
-
+//files to store all deduced end effector poses
+std::ofstream ee_poses;
 Eigen::Vector3d angles_stracter(Eigen::Matrix4d transform_l_ee_w){
     Eigen::Vector3d current_angles;
     double Roll, Pitch, Yaw;
@@ -66,24 +41,25 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "baxter_work_space");
     ros::NodeHandle node;
+    ros::Publisher pub_msg;
+    //open the file
+    ee_poses.open("baxter_workspace.csv");
     Eigen::VectorXd current_position(3);
     Eigen::Vector3d current_angles;
     bool continue_status = true;
     double angle_step = 0.05;
+    pub_msg = node.advertise<baxter_core_msgs::JointCommand>("/robot/limb/left/joint_command",1);
+    ros::AsyncSpinner my_spinner(4);
+    my_spinner.start();
+    usleep(1e6);
     //defining joints limits, mean values, ranges and other variables to avoid joint limits later
     Eigen::VectorXd qmin(7),qmax(7);
     qmin << -1.7016,-2.147,-3.0541,-0.05,-3.059,-1.5707,-3.059;
     qmax << 1.7016,1.047,3.0541,2.618,3.059,2.094,3.059;
-
     Eigen::VectorXd ee_pose(6), my_values(7);
-
     robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
     robot_model::RobotModelPtr robot_model = robot_model_loader.getModel();
-    //const moveit::core::LinkModel *right_arm = robot_model->getLinkModel("left_gripper");
-
     robot_state::RobotState my_robot_state(robot_model);
-    //const robot_state::JointModelGroup *joint_model_group = my_robot_state.getJointModelGroup("left_arm");
-
     std::vector <std::string> variable_names(7);
     variable_names[0] = "left_s0"; variable_names[1] = "left_s1"; variable_names[2] = "left_e0";
     variable_names[3] = "left_e1"; variable_names[4] = "left_w0"; variable_names[5] = "left_w1";
@@ -115,7 +91,18 @@ int main(int argc, char **argv)
                                     Eigen::Matrix4d transform_l_ee_w = f_trans_mat.matrix();
                                     current_angles = angles_stracter(transform_l_ee_w);
                                     ee_pose << current_position(0), current_position(1), current_position(2), current_angles(0),  current_angles(1), current_angles(2);
+                                    ee_poses << ee_pose(0) << "," << ee_pose(1) << "," << ee_pose(2) << "," << ee_pose(3) << "," << ee_pose(4) << "," << ee_pose(5) << "\n";
                                     std::cout << "end effector pose is: " << std::endl << ee_pose << std::endl;
+                                    /*baxter_core_msgs::JointCommand command_msg;
+                                    command_msg.mode = command_msg.POSITION_MODE;
+                                    command_msg.names.push_back("left_s0"); command_msg.names.push_back("left_s1"); command_msg.names.push_back("left_e0");
+                                    command_msg.names.push_back("left_e1"); command_msg.names.push_back("left_w0"); command_msg.names.push_back("left_w1");
+                                    command_msg.names.push_back("left_w2");
+                                    for(int i = 0; i < q.size(); i++)
+                                        command_msg.command.push_back(q[i]);
+                                    pub_msg.publish(command_msg);*/
+                                    std::cout << "press enter to try next joints configuration ..... " << std::endl;
+                                    //std::cin.ignore();
                                     //update joint left_w2 with the angle step (0.05 radian)
                                     q[6] = q[6] + angle_step;
                                 }
@@ -152,6 +139,7 @@ int main(int argc, char **argv)
         //when left_s0 reach its maximum value it means the whole joint space has been explored so finish the big while loop
         continue_status = false;
     }
+    ee_poses.close();
     std::cout << " ************************ I finished ******************************* " << std::endl;
     return 0;
 }
